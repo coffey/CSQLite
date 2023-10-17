9f769fbe253922bb77f72b4e8cc0c857da523a72
coffey <coffey@users.noreply.github.com>
2023-10-16 23:06:14 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -42,6 +42,7 @@ let package = Package(
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "CSQLite",
+			path: "Sources",
 			dependencies: [],
 			cSettings: [
 				// Compile-time options
