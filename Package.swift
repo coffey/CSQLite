c7cc085e6874458a6f6313bf7adf57d79b0e5f97
coffey <coffey@users.noreply.github.com>
2023-10-16 23:01:07 -0500

    Update Package.swift
    s

--- Package.swift
+++ Package.swift
@@ -27,6 +27,10 @@ import PackageDescription
 
 let package = Package(
 	name: "CSQLite",
+	platforms: [
+        .iOS(.v11),
+        .macOS(.v10_13),
+    	],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
